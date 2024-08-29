afe00f0e937a61863287812713fa4552c9dfcd25
Siddharth <sidd8214@gmail.com>
2024-08-30 00:14:40 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-qz04.onrender.com/", {
+			const socket = io("https://mern-chat-app-rgq1.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
